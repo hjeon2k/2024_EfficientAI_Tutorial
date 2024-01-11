64de745de97767d43fef1ba7982bb805d85f6aab
hjeon2k <hjeon2k@gmail.com>
2024-01-12 04:23:38 +0900

    Part2 Answer

--- models/resnet.py
+++ models/resnet.py
@@ -10,14 +10,12 @@ __all__ = ['resnet20', 'resnet32', 'resnet44', 'resnet56', 'resnet110', 'resnet1
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     """3x3 convolution with padding"""
-    # TODO feed the inputs
-    # return nn.Conv2d(in, out, kernel, stride=,
-                     # padding=, groups=, bias=, dilation=)
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
 
 def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
-    # TODO feed the inputs
-    # return nn.Conv2d(in, out, kernel, stride=, bias=)
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 
 class BasicBlock(nn.Module):
@@ -38,20 +36,21 @@ class BasicBlock(nn.Module):
         self.stride = stride
 
     def forward(self, x):
-        # TODO implement the downsample block regarding the residual connection & downsample function
-        # identity =
 
-        # out =
-        # out =
-        # out =
-        # out =
-        # out =
+        identity = x
 
-        # if self.downsample is not None:
-            # identity =
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
 
-        # out +=
-        # out =
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
 
         return out
 
@@ -69,16 +68,20 @@ class ResNet(nn.Module):
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU()
-
-        # TODO build the layers according to the elements in the 'layers' list regrading the strides
-        # self.layer1 = self._make_layer(block, 16, )
-        # self.layer2 = self._make_layer(block, 32, , stride=)
-        # self.layer3 = self._make_layer(block, 64, , stride=)
+        self.layer1 = self._make_layer(block, 16, layers[0], stride=1)
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(64, num_classes)
-
+        '''
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+        '''
     def _make_layer(self, block, planes, blocks, stride=1):
-        # TODO try to understand the code below (no need to modify)
         norm_layer = self._norm_layer
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
@@ -96,25 +99,23 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        # TODO implement the resnet block
-        # x =
-        # x =
-        # x =
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
 
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
 
-        # x =
+        x = self.avgpool(x)
         x = x.view(x.size(0), -1)
-        # x =
+        x = self.fc(x)
 
         return x
 
 def resnet20():
-    # TODO how many layers are assigned for resnet20?
-    # return ResNet(BasicBlock, [, , ])
-    pass
+    return ResNet(BasicBlock, [3, 3, 3])
 
 def resnet32():
     return ResNet(BasicBlock, [5, 5, 5])
